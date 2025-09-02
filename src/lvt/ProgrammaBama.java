c08acbec77ab038e0f5e5c5f4f596d49c726c799
KkManagalian <Dumbassyahoo2@gmail.com>
2025-09-02 12:07:13 +0300

    Ieviests cikls

--- src/lvt/ProgrammaBama.java
+++ src/lvt/ProgrammaBama.java
@@ -4,20 +4,24 @@ import java.util.Random;
 
 public class ProgrammaBama {
 
-	static void MestKaulinu() {
+	static void MestKaulinu(int reizes) {
 		
 		
 int skaitlis;
 Random rand = new Random();
 
+for(int i=1;i<=reizes;i++) {
 skaitlis = rand.nextInt(6)+1;
 
 System.out.println("Jusu skaitlis ir: "+skaitlis);
 	}
+
+
+	}
 	
 	public static void main(String[] args) {
 		
-MestKaulinu();
+MestKaulinu(5);
 		
 	}
 
