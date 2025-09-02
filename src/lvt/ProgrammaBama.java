8f12d52991c44410b9a34fd78167d50f0af8d779
KkManagalian <Dumbassyahoo2@gmail.com>
2025-09-02 12:14:28 +0300

    Jautajums

--- src/lvt/ProgrammaBama.java
+++ src/lvt/ProgrammaBama.java
@@ -1,18 +1,24 @@
 package lvt;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class ProgrammaBama {
 
 	public static void main(String[] args) {
 		
-int skaitlis;
+int skaitlis, reizes;
+Scanner scan = new Scanner(System.in);
+System.out.println("Cik reizes mest kaulinu?");
+reizes=scan.nextInt();
+
+for(int i=1; i<=reizes; i++){
 Random rand = new Random();
 
 skaitlis = rand.nextInt(6)+1;
 
 System.out.println("Jusu skaitlis ir: "+skaitlis);
-		
+}		
 		
 		
 
